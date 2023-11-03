c482b48a01da96721d57d6d087996b61d213631e
preetamrawat <psrawat1320@gmail.com>
2023-11-03 21:58:51 +0530

    created offer to mail component

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -3,6 +3,13 @@ import './NewsLetter.css'
 
 export const NewsLetter = () => {
   return (
-    <div>NewsLetter</div>
+    <div className='newsletter'>
+      <h1>Get Exclusive Offers On Your Email</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
+      <div>
+        <input type='email' placeholder='Your Email id'/>
+        <button>Subscribe</button>
+      </div>
+    </div>
   )
 }
