6402f98342c06a33a4a13693b9e6010788fdd5af
preetamrawat <psrawat1320@gmail.com>
2023-11-03 16:44:57 +0530

    addedt new collection items

---
+++ src/Components/Assets/new_collection.js
@@ -0,0 +1,70 @@
+import p1_img from './newcollection_img1.png'
+import p2_img from './newcollection_img2.png'
+import p3_img from './newcollection_img3.png'
+import p4_img from './newcollection_img4.png'
+import p5_img from './newcollection_img5.png'
+import p6_img from './newcollection_img6.png'
+import p7_img from './newcollection_img7.png'
+import p8_img from './newcollection_img8.png'
+
+
+const new_collection_data=[
+    {
+        id:1,
+        name:"Black and Red Jacket",
+        image:p1_img,
+        new_price:20.00,
+        old_price:50.00
+    },
+    {
+        id:2,
+        name:"Kids TrackSuit",
+        image:p2_img,
+        new_price:59.00,
+        old_price:90.00
+    },
+    {
+        id:3,
+        name:"Men Grey Hoodie",
+        image:p3_img,
+        new_price:40.00,
+        old_price:70.00
+    },
+    {
+        id:4,
+        name:"Women Jacket",
+        image:p4_img,
+        new_price:40.00,
+        old_price:100.00
+    },
+    {
+        id:5,
+        name:"Kids Woollen Dress ",
+        image:p5_img,
+        new_price:70.00,
+        old_price:120.00
+    },
+    {
+        id:6,
+        name:"Women Grey Sweater",
+        image:p6_img,
+        new_price:80.00,
+        old_price:100.00
+    },
+    {
+        id:7,
+        name:"Men Offwhite Hoodie",
+        image:p7_img,
+        new_price:67.00,
+        old_price:99.00
+    },
+    {
+        id:8,
+        name:"Women Olive Green Jacket",
+        image:p8_img,
+        new_price:79.00,
+        old_price:199.00
+    }
+]
+
+export default new_collection_data
--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import './NewCollection.css'
+import new_collection_data from '../Assets/new_collection.js'
+import Item from '../Item/Item'
+import './NewCollections.css'
 
 const NewCollections = () => {
   return (
-    <div>NewCollections</div>
+    <div className='new-collections'>
+     <h1>NEW COLLECTIONS</h1>
+     <hr/>
+     <div className='collections'>
+      {new_collection_data.map((item,i)=>{
+        return <Item  key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+      })}
+     </div>
+    </div>
   )
 }
 
