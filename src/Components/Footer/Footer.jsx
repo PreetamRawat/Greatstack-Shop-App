857abe1ec90579d50159c0563ecf11361026f58e
preetamrawat <psrawat1320@gmail.com>
2023-11-03 22:22:25 +0530

    created footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,34 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className='footer'></div>
+    <div className='footer'>
+       <div className='Footer-logo'>
+        <img src='' alt=''/>
+        <p>SHOPPER</p>
+       </div>
+       <ul className='footer-links'>
+        <li>Company</li>
+        <li>Products</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li>Contact</li>
+       </ul>
+       <div className='footer-social-icons'>
+        <div className='footer-icons-container'>
+            <img src='' alt=''/>
+        </div>
+        <div className='footer-icons-container'>
+            <img src='' alt=''/>
+        </div>
+        <div className='footer-icons-container'>
+            <img src='' alt=''/>
+        </div>
+       </div>
+       <div className='"footer-copyright'>
+        <hr />
+        <p>Copyright @2023 - All Right Reserved</p>
+       </div>
+    </div>
   )
 }
 
