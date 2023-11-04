175048b4d5cc57d121a81894108c39730aec15d9
preetamrawat <psrawat1320@gmail.com>
2023-11-04 12:21:23 +0530

    created footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,37 +1,39 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import { SocialIcon } from "react-social-icons";
+import brandlogo from "../Assets/brandlogo.png";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-       <div className='Footer-logo'>
-        <img src='' alt=''/>
+    <div className="footer">
+      <div className="footer-logo">
+        <img src={brandlogo} alt="loading.." />
         <p>SHOPPER</p>
-       </div>
-       <ul className='footer-links'>
+      </div>
+      <ul className="footer-links">
         <li>Company</li>
         <li>Products</li>
         <li>Offices</li>
         <li>About</li>
         <li>Contact</li>
-       </ul>
-       <div className='footer-social-icons'>
-        <div className='footer-icons-container'>
-            <img src='' alt=''/>
+      </ul>
+      <div className="footer-social-icons">
+        <div className="footer-icons-container">
+          <SocialIcon network="whatsapp" />
         </div>
-        <div className='footer-icons-container'>
-            <img src='' alt=''/>
+        <div className="footer-icons-container">
+          <SocialIcon network="linkedin" />
         </div>
-        <div className='footer-icons-container'>
-            <img src='' alt=''/>
+        <div className="footer-icons-container">
+          <SocialIcon network="github" />
         </div>
-       </div>
-       <div className='"footer-copyright'>
+      </div>
+      <div className='footer-copyright'>
         <hr />
         <p>Copyright @2023 - All Right Reserved</p>
-       </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
