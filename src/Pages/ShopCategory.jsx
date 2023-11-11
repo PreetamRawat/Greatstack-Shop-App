403afdf7e12735f98232311c036724d648b8abc2
preetamrawat <psrawat1320@gmail.com>
2023-11-11 17:18:31 +0530

    import item component in shop category

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,13 +1,37 @@
 import React, { useContext } from 'react'
 import  './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Item from '../Components/Item/Item.jsx'
 
 const ShopCategory = (props) => {
   const {all_collection} = useContext(ShopContext)
- 
+//  console.log(all_collection);
+
   return (
     <div className='shop-category'>
+    <h1>chjchhdh</h1>
       <img src={props.banner} alt=''/>
+      <div className='shopcategory-indexSort'>
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className='shopcategory-sort'>
+          Sort by <img src={ArrowDropDownIcon} alt='' />
+        </div>
+      </div>
+      <div className='shopcategory-products'>
+        {
+          all_collection.map((item,i)=>{
+            if(props.category===item.category){
+              return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            }
+            else{
+              return null;
+            }
+          })
+        }
+      </div>
     </div>
   )
 }  
