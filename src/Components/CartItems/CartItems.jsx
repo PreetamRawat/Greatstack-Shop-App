eedfefc2a615aada64c2267b799b931a27a807b8
preetamrawat <psrawat1320@gmail.com>
2023-11-17 23:37:52 +0530

    adding css to promo box

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import {ShopContext} from '../../Context/ShopContext'
 import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
-    const {all_product,cartItems,removeFromCart} = useContext(ShopContext)
+    const {getTotalCartAmount,all_product,cartItems,removeFromCart} = useContext(ShopContext)
   return (
     <div className='cartitems'>
       <div className="cartitems-format-main">
@@ -34,12 +34,12 @@ const CartItems = () => {
         return null;
       })}
       <div className="cartitems-down">
-        <div className="cartitems-total">
+        <div className="c">
           <h1>Cart Total</h1>
           <div>
-            <div className="cartitems-totoal-item">
+            <div className="cartitems-total-item">
               <p>Subtotal</p>
-              <p>${0}</p>
+              <p>${getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cartitems-total-item">
@@ -48,7 +48,7 @@ const CartItems = () => {
             <hr />
             <div className="cartitems-total-item">
               <h3>Total</h3>
-              <h3>${0}</h3>
+              <h3>${getTotalCartAmount()}</h3>
             </div>
           </div>
           <button>PROCEED TO CHECKOUT</button>
